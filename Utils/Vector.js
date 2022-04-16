8fd01690dea49263be7dd6a8890d3a364756b109
David <david.turley@live.nl>
2022-04-17 00:25:10 +0200

    Updated Get Angle

--- Utils/Vector.js
+++ Utils/Vector.js
@@ -2,8 +2,19 @@ function GetVectorToObject(from, to){
     return createVector(to.x - from.x, to.y - from.y, to.z - from.z);
 }
 
+
 function GetAngleToObject(from, to){
-    Math.atan2(y, x)
+    return (Math.atan2(to.y - from.y, to.x - from.x) * 180 / Math.PI) * -1;
+
+    //               Angle returned as:
+    //                      90
+    //            135                45
+    //
+    //       180          Origin           0
+    //
+    //           -135                -45
+    //
+    //                     -90
 }
 
 function InvertVectorX(vector){

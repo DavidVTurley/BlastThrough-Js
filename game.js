4852c4c0f8bbeaf810097f20aad9cf47059d6239
David <david.turley@live.nl>
2022-04-21 16:20:44 +0200

    Updated tiles

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 let PlayerPaddle;
 let Ball;
-let Tile;
+let Tiles;
+let PowerUps = [];
 const CanvasWidth = 500;
 const CanvasHeight = 500;
 
@@ -9,24 +10,25 @@ function setup(){
     createCanvas(CanvasWidth, CanvasHeight);
     PlayerPaddle = new Player(createVector(CanvasWidth / 2, CanvasHeight - 25, 0), 100, 20, 3, true);
     Ball = new BallObject(createVector(250, 300, 0), 15, 3);
-    Tile = [];
+    Tiles = [];
     for(let y = 1; y <= 10; y++){
-        Tile.push([]);
+        Tiles.push([]);
         for(let x = 1; x <= 10; x ++){
-            Tile[y-1].push(new Block(createVector(40*x + 4 + 20, 20*y), 40, 20));
+            Tiles[y-1].push(new Block(createVector(40*x + 4 + 20, 20*y), 40, 20));
         }
     }
 }
 
 function draw(){
     background(220);
-    for(let y = 0; y < Tile.length; y++){
-        for(let x = 0; x < Tile[y].length; x ++){
-            theTile = Tile[y][x];
-            if(Ball.CheckCollision(theTile)){
-                Ball.RespondCollisionWithTile(theTile);
-            }
-
+    PlayerPaddle.Update();
+    PlayerPaddle.Draw();
+    Ball.Update(PowerUps, PlayerPaddle, Tiles);
+    Ball.Draw();
+    
+    for(let y = 0; y < Tiles.length; y++){
+        for(let x = 0; x < Tiles[y].length; x ++){
+            theTile = Tiles[y][x];
             theTile.Draw();
         }
     } 

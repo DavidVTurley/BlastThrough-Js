8c21fb19f0084ab9cdc99228f32fc3be7c7bebc4
David <david.turley@live.nl>
2022-04-23 01:28:29 +0200

    Updated Power up creation and colour

--- Objects/PowerUp.js
+++ Objects/PowerUp.js
@@ -6,6 +6,58 @@ class PowerUp extends RectangleObject{
         this.CleanUp = false;
     }
 
+    static CreatePowerUp(position){
+        let r = random(0, 6);
+        if(r < 4){
+            console.log("PowerupCreated")
+            return new PowerUp(CopyVector(position), 40, 20, true, createVector(0, 1, 0), 2, "SizeUp", true);
+        }
+        if(r < 6){
+            console.log("PowerupCreated")
+            return new PowerUp(CopyVector(position), 40, 20, true, createVector(0, 1, 0), 2, "SizeDown", true);
+        }
+        if(r < 4){
+            console.log("PowerupCreated")
+            return new PowerUp(CopyVector(position), 40, 20, true, createVector(0, 1, 0), 2, "SizeUp", true);
+        }
+        if(r < 4){
+            console.log("PowerupCreated")
+            return new PowerUp(CopyVector(position), 40, 20, true, createVector(0, 1, 0), 2, "SizeUp", true);
+        }
+        if(r < 4){
+            console.log("PowerupCreated")
+            return new PowerUp(CopyVector(position), 40, 20, true, createVector(0, 1, 0), 2, "SizeUp", true);
+        }
+    }
+
+    Draw(){
+        switch (this.PowerUpType) {
+            case "SizeUp": 
+                DrawRectangleFromGameObject(this, color(0, 255, 70), true)
+                break;
+
+            case "SizeDown": 
+                DrawRectangleFromGameObject(this, color(255,0,0), true);
+                break;
+
+            case "SizeUp": 
+                DrawRectangleFromGameObject(this, color(0, 255, 70), true);
+                break;
+
+            case "SizeUp": 
+                DrawRectangleFromGameObject(this, color(0, 255, 70), true);
+                break;
+
+            case "SizeUp": 
+                DrawRectangleFromGameObject(this, color(0, 255, 70), true);
+                break;
+            default:
+                DrawRectangle(this, color(255, 255, 255))
+                break;
+        }
+
+    }
+
     Update(player) {
         if(!this.HitBox) return false;
         if(this.DetectColisionWithSidesOfCanvas()){
